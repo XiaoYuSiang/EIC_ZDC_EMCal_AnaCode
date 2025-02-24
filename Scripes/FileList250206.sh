cdb104813d1c5f3f5c02bc719f4759c6119403ee
XiaoYuSiang <105832014+XiaoYuSiang@users.noreply.github.com>
2025-02-25 03:13:34 +0800

    CLEAR

--- Scripes/FileList250206.sh
+++
@@ -1,9 +0,0 @@
-root -b -q -l CompilarV2.C
-sleep 0 &&root -l -b -q Read.C\("\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/01_CosmicRay_HV408V_FV280_300_LG_x60_020735.778LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/01_CosmicRay_HV408V_FV280_300_LG_x60_020735.778LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/01_CosmicRay_HV30V_FV320_020735.778Monitor1.bin\"","\"\"","\"\"",31\) &
-sleep 0 &&root -l -b -q Read.C\("\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/02_CosmicRay_HV200V_VF290_330_LG_x1_123858.773LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/02_CosmicRay_HV200V_VF290_330_LG_x1_123858.773LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/02_CosmicRay_HV30V_VF290_LG_x4_123858.773Monitor1.bin\"","\"\"","\"\"",31\) &
-sleep 0 &&root -l -b -q Read.C\("\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/03_CosmicRay_HV210V_VF310_360_LG_x1_160946.486LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/03_CosmicRay_HV210V_VF310_360_LG_x1_160946.486LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/03_CosmicRay_HV30V_VF280_LG_x4_160946.486Monitor1.bin\"","\"\"","\"\"",31\) &
-sleep 0 &&root -l -b -q Read.C\("\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/04_CosmicRay_HV388V_FV280_300_LG_x20_191512.683LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/04_CosmicRay_HV388V_FV280_300_LG_x20_191512.683LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/04_CosmicRay_HV30V_FV320_191512.683Monitor1.bin\"","\"\"","\"\"",31\) &
-sleep 0 &&root -l -b -q Read.C\("\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/05_CosmicRay_HV388V_FV250_270_LG_x4_222515.710LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/05_CosmicRay_HV388V_FV250_270_LG_x4_222515.710LYSO.bin\"","\"/data8/ZDC/EMCal/LYSO2ABMCosmic/250206/05_CosmicRay_HV30V_FV320_222515.710Monitor1.bin\"","\"\"","\"\"",31\) &
-wait
-echo "Finish the scripe"
-
--- TBBO_PWO/FileList250219_PWO.sh
+++
@@ -1,4 +0,0 @@
-sleep 0 &&root -l -b -q Read.C\("\"/data8/ZDC/EMCal/BeamTest/BeamOn0220/Run2041_297MeV_HV17_VF650_650_x4_Pos145mm_-346mm_0mm_082806.842PbWO4.bin\"","\"/data8/ZDC/EMCal/BeamTest/BeamOn0220/Run2041_297MeV_HV17_VF650_650_x4_Pos145mm_-346mm_0mm_082806.842PbWO4.bin\"","\"/data8/ZDC/EMCal/BeamTest/BeamOn0220/Run2041_297MeV_HV16_VF320_320_x4_Pos145mm_-346mm_0mm_082805.484Monitor1.bin\"","\"/data8/ZDC/EMCal/BeamTest/BeamOn0220/Run2041_297MeV_HV17_VF320_320_x4_Pos145mm_-346mm_0mm_082805.484Monitor2.bin\"","\"\"",1\) &
-wait
-echo "Finish the scripe"
-
